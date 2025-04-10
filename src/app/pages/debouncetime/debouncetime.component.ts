adf5e81bc3a5364942e7cc09420d042fb9301eea
Akash <akash19102001@gmail.com>
2025-04-10 15:39:57 +0530

    comments on debounce and throttle

--- src/app/pages/debouncetime/debouncetime.component.ts
+++ src/app/pages/debouncetime/debouncetime.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { fromEvent, map , debounceTime, Observable } from 'rxjs';
+import { fromEvent, map, debounceTime, Observable, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-debouncetime',
@@ -9,8 +9,8 @@ import { fromEvent, map , debounceTime, Observable } from 'rxjs';
 export class DebouncetimeComponent {
   @ViewChild('inputField') inputEl!: ElementRef;
 
-  typingObservable!:Observable<string>
-  inputValue!:string;
+  typingObservable!: Observable<string>;
+  inputValue!: string;
 
   constructor() {}
 
@@ -18,14 +18,22 @@ export class DebouncetimeComponent {
     this.debounceFunction();
   }
 
+  //create an observable stream out of an input event
+  //map to transform the data such that instead of the event , we are getting the value from the target
+  //add debpuncetime to it with argument as the delay
+
   debounceFunction() {
     this.typingObservable = fromEvent(this.inputEl.nativeElement, 'input').pipe(
-      map((event:any) => (event.target as HTMLInputElement).value),
-      debounceTime(1000)
+      map((event: any) => (event.target as HTMLInputElement).value),
+      debounceTime(1000),
+
+      //distinct until change means it wont hidner if the new emitted value is same as that of the previous value
+      distinctUntilChanged()
     );
 
+    //while we subscribe , we will only get the devounced stream rather than the raw stream
     this.typingObservable.subscribe((res: any) => {
-      this.inputValue=res;
+      this.inputValue = res;
     });
   }
 }
--- src/app/pages/throttletime/throttletime.component.ts
+++ src/app/pages/throttletime/throttletime.component.ts
@@ -1,16 +1,16 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { fromEvent, map , throttleTime, Observable } from 'rxjs';
+import { fromEvent, map, throttleTime, Observable , distinctUntilChanged} from 'rxjs';
 
 @Component({
   selector: 'app-throttletime',
   templateUrl: './throttletime.component.html',
-  styleUrl: './throttletime.component.scss'
+  styleUrl: './throttletime.component.scss',
 })
 export class ThrottletimeComponent {
   @ViewChild('inputField') inputEl!: ElementRef;
 
-  typingObservable!:Observable<string>
-  inputValue!:string;
+  typingObservable!: Observable<string>;
+  inputValue!: string;
 
   constructor() {}
 
@@ -18,14 +18,20 @@ export class ThrottletimeComponent {
     this.debounceFunction();
   }
 
+  //create an observable stream out of an input event
+  //map to transform the data such that instead of the event , we are getting the value from the target
+  //add throttle time to it with argument as the delay
   debounceFunction() {
     this.typingObservable = fromEvent(this.inputEl.nativeElement, 'input').pipe(
-      map((event:any) => (event.target as HTMLInputElement).value),
-      throttleTime(1000)
+      map((event: any) => (event.target as HTMLInputElement).value),
+      throttleTime(1000),
+
+      //distinct until change means it wont hidner if the new emitted value is same as that of the previous value
+      distinctUntilChanged()
     );
 
     this.typingObservable.subscribe((res: any) => {
-      this.inputValue=res;
+      this.inputValue = res;
     });
   }
 }
